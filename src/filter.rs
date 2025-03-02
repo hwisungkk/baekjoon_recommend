ea71d328c4a1289572ceaf6c8cf11a40229f7e20
railgun919 <50361172+railgun919@users.noreply.github.com>
2025-03-02 22:04:05 +0900

    baekjoon

--- src/filter.rs
+++ src/filter.rs
@@ -3,6 +3,6 @@ pub fn apply_filters(problems: Vec<(String, i32)>, solved_problems: &[i32]) -> V
         .filter(|(title, id)| {
             let korean = title.chars().any(|c| c >= '\u{AC00}' && c <= '\u{D7A3}');  //한글 문제들만 받도록
             !solved_problems.contains(id) && korean
-        })
+        })  
         .collect()
 }
